8f9562f61a18337baf2f07dea7e8cf67caa6e27b
Zilio Bruno <bruno.zilio@in-tact.fr>
2022-07-06 12:30:30 +0200

    migration continue

--- database/migrations/1657039951500_plannings.ts
+++ database/migrations/1657039951500_plannings.ts
@@ -1,3 +1,4 @@
+import { DateTime } from 'luxon';
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class Plannings extends BaseSchema {
@@ -6,7 +7,13 @@ export default class Plannings extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.timestamps(true)
+      table.integer('user_project_id').unsigned().notNullable().references('id').inTable('user_projects')
+
+      table.dateTime('day_date')
+      table.float('day_quantity').nullable
+
+      table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
+      table.timestamp('updated_at', { useTz: true })
     })
   }
 
